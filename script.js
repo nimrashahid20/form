daddc75d20c608c11598e41e0670d1b6cf291142
Nimra Shahid <nimrashahid02007@gmail.com>
2024-09-09 15:45:32 +0500

    form

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// const div_element = document.querySelector("div")
+// console.log(div_element)
+// let att = div_element.getAttribute("container")
+// console.log(att)
+
+// // let para = document.querySelector("p")
+// // console.log(para.setAttribute('id',"paraghraph_one"))
+
+
+// const element = document.getElementById("myh1")
+// let text = element.getAttribute("class"); 
+// document.getElementById("demo").innerHTML = text;
+
+// let para = document.querySelector("p")
+// para.style.backgroundColor = "blue"
+// para.style.color = "pink"
+
+let newbtn = document.createElement("h1")
+newbtn.innerText = "My Heading"
+console.log(newbtn)
+
+let mydiv = document.querySelector("div")
+mydiv.before(newbtn)
+
+
+
+
+
+
+
+
+
+
+
+
